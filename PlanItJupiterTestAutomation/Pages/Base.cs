b5fb6ae36397601e6d157ab6514b606861f922ba
Chinthaka Samarasinghe <39684266+AluthD@users.noreply.github.com>
2023-10-09 23:01:12 +1100

    Adding reports (#1)

--- PlanItJupiterTestAutomation/Pages/Base.cs
+++ PlanItJupiterTestAutomation/Pages/Base.cs
@@ -1,5 +1,10 @@
-﻿using OpenQA.Selenium;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using LivingDoc.Dtos;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V115.Page;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using PlanItJupiterTestAutomation.Utilities;
@@ -16,10 +21,30 @@ namespace PlanItJupiterTestAutomation.Pages
     public class Base
     {
         protected IWebDriver driver;
+        public ExtentReports extent;
+        public ExtentTest reportingTest;
+
+        // Report File
+        [OneTimeSetUp] 
+        public void Setup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            String reportPath = projectDirectory + "//index.html";
+            var htmlReporter = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports(); 
+            extent.AttachReporter(htmlReporter);
+            extent.AddSystemInfo("Environment", "QA");
+            extent.AddSystemInfo("UserName", "Chinthaka Samarasinghe");
+
+        }
 
         [SetUp]
         public void StartBrowser()
         {
+            // Get the test name when executing
+            reportingTest = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+
             BrowserExtention browserExtention = new BrowserExtention();
 
             string browserName = ConfigurationManager.AppSettings["browser"];
@@ -68,9 +93,34 @@ namespace PlanItJupiterTestAutomation.Pages
         [TearDown]
         public void TearDownBrowser()
         {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+
+       
+            if (status == TestStatus.Failed)
+            {
+
+                reportingTest.Fail("Test Failed");
+                reportingTest.Log(Status.Fail, "test failed with log trace: " + stackTrace);
+            }
+            else if(status == TestStatus.Passed)
+            {
+                reportingTest.Pass("Test Passed");
+            }
+
+            extent.Flush();
             driver.Close();
             //driver.Quit();
         }
+
+        //public MediaEntityModelProvider CaptureScreenShot(String screenShotName)
+        //{
+        //    ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+        //    var screenShot = takesScreenshot.GetScreenshot().AsBase64EncodedString;
+        
+        //    return  MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenShot, screenShotName).Build;
+
+        //}
 
     }
 }
